c9823f2f176136ddf30977ff33d86c45ae08597b
nagachetana-008 <245123748008@mvsrec.edu.in>
2026-04-10 13:24:25 +0530

    Update filename.c

--- filename.c
+++ filename.c
@@ -1,6 +1,25 @@
-#include<stdio.h>
-void main()
-{
-	printf("hello");
-}
 
+#include <stdio.h>
+
+int main() {
+    int num = 17;  // change the number here
+    int i, flag = 0;
+
+    if (num <= 1) {
+        flag = 1;
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+    }
+
+    if (flag == 0)
+        printf("%d is a Prime Number", num);
+    else
+        printf("%d is not a Prime Number", num);
+
+    return 0;
+}
