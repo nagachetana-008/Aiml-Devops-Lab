c207ca91878f71b9b70745bbc597e0b566bb4856
nagachetana-008 <245123748008@mvsrec.edu.in>
2026-06-28 16:38:29 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println("Addition:"+(a+b));
+    System.out.println("Subtraction:"+(a-b));
+    System.out.println("Multiplication:"+(a*b));
+    System.out.println("Division:"+(a/b));
+  }
+}
+    
