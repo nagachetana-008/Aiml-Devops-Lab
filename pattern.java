dffa32d21bdbdd478cf70a8a0c65a22c9030b2c2
nagachetana-008 <245123748008@mvsrec.edu.in>
2026-06-28 14:48:49 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,7 +1,7 @@
 public class Pattern{
 	public static void main(String[] args){
-			for(i=1;i<=5;i++){
-				for(j=1;j<=i;j++){
+			for(int i=1;i<=5;i++){
+				for(int j=1;j<=i;j++){
 					System.out.println("*");
 				}
 			System.out.println();
